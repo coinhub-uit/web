f6ea4a5a9323489cce245c81bf48d32d6efe811b
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-03-30 00:33:11 +0700

    fix(useFetch): handle if session isn't available before fetch

--- src/lib/hooks/useFetch.tsx
+++ src/lib/hooks/useFetch.tsx
@@ -3,29 +3,26 @@
 import useSWR from 'swr';
 import { useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
-import { API_URL } from '@/constants/api-urls';
 
-async function fetcher(
-  url: string,
-  accessToken: string,
-  init?: RequestInit,
-): Promise<Response> {
+async function fetcher(url: string, accessToken: string, init?: RequestInit) {
   if (!init) {
     init = {};
   }
   init.headers = {
     Authorization: `Bearer ${accessToken}`,
-    'Access-Control-Allow-Origin': API_URL,
   };
-  return await fetch(url, init);
+  return await (await fetch(url, init)).json();
 }
 
 export default function useFetch(url: string, init?: RequestInit) {
-  const { data } = useSession({
+  const { data, status } = useSession({
     required: true,
     onUnauthenticated() {
       redirect('api/auth/signin');
     },
   });
-  return useSWR(url, (url) => fetcher(url, data!.user.accessToken, init));
+  console.log('data', data);
+  return useSWR(status === 'authenticated' ? url : null, (url) =>
+    fetcher(url, data!.user.accessToken, init),
+  );
 }
